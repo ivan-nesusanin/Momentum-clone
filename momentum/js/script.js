827105aace6a7ba39f441aedf9f380b59776423d
ivan-nesusanin <l75x5@yandex.ru>
2021-10-23 23:21:52 +0300

    feat: add weather block

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -3,3 +3,4 @@ import * as time from './time.js';
 import * as greeting from './greeting.js';
 import * as name from './greeting_name.js';
 import * as slider from './slider.js';
+import * as weather from './weather.js';
---
+++ momentum/js/weather.js
@@ -0,0 +1,47 @@
+const weatherIcon = document.querySelector('.weather-icon');
+const TEMP = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const WIND = document.querySelector('.wind');
+const HUMIDITY = document.querySelector('.humidity');
+const CITY = document.querySelector('.city');
+const weatherError = document.querySelector('.weather-error');
+
+CITY.value = 'Minsk';
+
+async function getWeather() {
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${CITY.value}&lang=en&appid=f05d8ecaf36987de897e308b1f216c48&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json();
+    weatherIcon.className = 'weather-icon owf';
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    TEMP.textContent = `${Math.round(data.main.temp)}°C`;
+    weatherDescription.textContent = data.weather[0].description;
+    WIND.textContent = `Wind speed: ${Math.round(data.wind.speed)} m/s`;
+    HUMIDITY.textContent = `Humidity: ${Math.round(data.main.humidity)}%`;
+  } catch (error) {
+    weatherError.textContent = `Error! City not found for "${CITY.value}"!`;
+    weatherDescription.innerHTML = '';
+    TEMP.innerHTML = '';
+    WIND.innerHTML = '';
+    HUMIDITY.innerHTML = '';
+  }
+}
+getWeather()
+
+// погода для выбранного города
+CITY.addEventListener('change', changeCity);
+function changeCity(event) {
+  if (event.ketCode === 13 || CITY.blur) {
+    getWeather();
+  }
+}
+
+// сохранение данных в local storage
+function getLocalStorage() {
+  if (localStorage.getItem('city')) {
+    CITY.value = localStorage.getItem('city');
+  }
+  getWeather();
+}
+window.addEventListener('load', getLocalStorage)
