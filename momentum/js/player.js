df869f5ccef8d8f34208c5a5a91e98730e50e37d
ivan-nesusanin <l75x5@yandex.ru>
2021-10-25 16:03:11 +0300

    feat: add advance audio player

--- momentum/js/player.js
+++ momentum/js/player.js
@@ -7,6 +7,14 @@ const playPrev = document.querySelector('.play-prev');
 const playListContainer = document.querySelector('.play-list')
 let isPlay = false;
 let playNum = 0;
+// const for ADVANCE PLAYER
+const volumeBar = document.querySelector('.volume-bar');
+const volume = document.querySelector('.volume');
+const progressBar = document.querySelector('.progress-bar');
+const trackName = document.querySelector('.track-name');
+let current = document.querySelector('.current-time');
+const duration = document.querySelector('.duration');
+const progress = document.querySelector('.progress-container');
 
 // создаем плейлист
 for (let i = 0; i < playList.length; i++) {
@@ -22,10 +30,13 @@ const playItem = document.querySelectorAll('.play-item');
 PLAY.addEventListener('click', playAudio);
 function playAudio() {
   audio.src = playList[playNum].src;
-  playItem[playNum].classList.add('item-active');
+  playItem[playNum].classList.add('item-active');         //выделение активного трека
+  trackName.textContent = playItem[playNum].innerHTML;    //вывод названия трека над прогресс-баром
+  duration.textContent = playList[playNum].duration;      //общая продолжительность трека
   if (!isPlay) {
     audio.play();
     isPlay = true;
+    audio.currentTime;
   } else {
     audio.pause();
     isPlay = false;
@@ -67,4 +78,67 @@ function prevTrack() {
   }
   isPlay = false;
   playAudio();
+}
+
+// ADVANCE PLAYER
+
+// VOLUME AND MUTE
+volumeBar.addEventListener('change', handleRageUpdate);
+volumeBar.addEventListener('mousemove', handleRageUpdate);
+volumeBar.addEventListener('mousemove', handleMute);
+
+function handleRageUpdate() {
+  audio[this.name] = this.value;
+}
+
+function handleMute() {
+  if (volumeBar.value === '0') {
+    volume.classList.add('mute');
+    volume.classList.remove('volume');
+  } else {
+    volume.classList.add('volume');
+    volume.classList.remove('mute');
+  }
+}
+
+volume.addEventListener('click', doMute);
+
+function doMute() {
+  if (audio.muted) {
+    audio.muted = false;
+    volume.classList.add('volume');
+    volume.classList.remove('mute');
+  } else {
+    audio.muted = true;
+    volume.classList.add('mute');
+    volume.classList.remove('volume');
+  }
+}
+
+// PROGRESS-BAR
+
+audio.addEventListener('timeupdate', moveAudioProgress);
+progressBar.addEventListener('click', rewindProgress);
+
+function moveAudioProgress() {
+  progressBar.value = (audio.currentTime / audio.duration) * 100;
+  let minutes = Math.floor(audio.currentTime / 60);
+  let seconds = Math.floor(audio.currentTime % 60);
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+  current.textContent = `${minutes}:${seconds}`;
+  if (audio.currentTime === audio.duration) {
+    playItem[playNum].classList.remove('item-active');
+    playNum++;
+    isPlay = false;
+    playAudio();
+  }
+}
+
+function rewindProgress(event) {
+  audio.currentTime = (event.offsetX / progress.offsetWidth) * audio.duration;
 }
--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,10 +1,9 @@
 import * as time from './time.js';
-// import * as date from './date.js';
 import * as greeting from './greeting.js';
 import * as name from './greeting_name.js';
 import * as slider from './slider.js';
 import * as weather from './weather.js';
 import * as quotes from './quote.js';
 import * as player from './player.js';
-import playList from './playList.js';
+
 
